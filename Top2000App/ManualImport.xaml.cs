0e151712af7fe9ed8691f4cfc5e576cd237011a0
NaamanNumansen <numan@NAAMANLAPPIE>
2017-06-12 10:15:18 +0200

    manual import

--- Top2000App/ManualImport.xaml.cs
+++ Top2000App/ManualImport.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,62 @@ namespace Top2000App
         public ManualImport()
         {
             InitializeComponent();
+        }        
+
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            DataConnection();
         }
+
+        #region Dataconnectie methode
+
+        //methode maken voor automatische connectie
+        public void DataConnection()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"Server = (localdb)\mssqllocaldb;");
+            sb.Append("Database = TOP2000;");
+            sb.Append("User Id = i5ao1;");
+            sb.Append("Password = test;");
+            string sr = "";
+            string cs = sb.ToString();
+            SqlConnection conn = new SqlConnection(sr);
+            SqlCommand cmd;
+            conn.ConnectionString = cs;
+
+            int position = Convert.ToInt32(txtPos.Text);
+            string songTitel = txtTitel.Text;
+            string artistName = txtArtistN.Text;
+            int songYear = Convert.ToInt32(txtYear.Text);
+            string dateTime = txtTime.Text;
+
+            try
+            {
+                conn.Open();
+                string st = string.Format("BEGIN TRANSACTION DECLARE @POS int; DECLARE @SONGtitel nvarchar(100); DECLARE @ARTISTname nvarchar(100); DECLARE @SONGyear int; DECLARE @SONGtime datetime; SET @POS = { 0 }; SET @SONGtitel = { 1 }; SET @ARTISTname = { 2 }; SET @SONGyear = { 3 }; SET @SONGtime = { 4 }; INSERT INTO Lijst(positie) VALUES(@POS); INSERT INTO Song(titel, jaar, tijd) VALUES(@SONGtitel, @SONGyear, @SONGtime); INSERT INTO Artiest(naam) VALUES(@ARTISTname); COMMIT", position, songTitel, artistName, songYear, dateTime);
+                cmd = new SqlCommand(st, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                {
+                    conn.Close();
+                }
+            }
+        }
+        #endregion
+
     }
 }
