9f6426f72663efe0659f1142fbc153cebaec766c
NaamanNumansen <numan@NAAMANLAPPIE>
2017-06-23 10:06:14 +0200

    Comments of manual import

--- Top2000App/ManualImport.xaml.cs
+++ Top2000App/ManualImport.xaml.cs
@@ -19,19 +19,32 @@ namespace Top2000App
     /// <summary>
     /// Interaction logic for ManualImport.xaml
     /// </summary>
+    /// <seealso cref="System.Windows.Window" />
+    /// <seealso cref="System.Windows.Markup.IComponentConnector" />
     public partial class ManualImport : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ManualImport"/> class.
+        /// </summary>
         public ManualImport()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the Click event of the btnSubmit control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
 
             CheckInput();
         }
 
+        /// <summary>
+        /// Checks the input and displays messagebox if the input is not right
+        /// </summary>
         public void CheckInput()
         {
             int n;
@@ -61,6 +74,9 @@ namespace Top2000App
 
         #region Import Manual methode
 
+        /// <summary>
+        /// Imports a new row into the database.
+        /// </summary>
         public void DataConnection()
         {
             StringBuilder sb = new StringBuilder();
@@ -89,8 +105,7 @@ namespace Top2000App
                 DataTable table = new DataTable();
                 table.Load(reader);
                 string item = table.Rows[0].ItemArray[0].ToString();
-                // artiest bestaad
-
+                // If the artist exists
                 if (item == "1")
                 {
                     conn.Open();
@@ -100,6 +115,7 @@ namespace Top2000App
                     DataTable tableone = new DataTable();
                     table.Load(reader);
                 }
+                // If the artist does not exist
                 else {
                     conn.Open();
                     string sttwo = string.Format("EXEC[dbo].[importManual] @naam = N'{0}', @titel = N'{1}', @jaar = {2}, @tijd = N'{3}', @top2000jaar = {4}, @positie = {5}", artistName, songTitel, songYear , dateTime, posYear, position);
@@ -130,36 +146,71 @@ namespace Top2000App
 
         #region txtbox clear and fill tips
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtPos control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtPos_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtPos.Clear();
         }
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtTitel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtTitel_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtTitel.Clear();
         }
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtArtistN control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtArtistN_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtArtistN.Clear();
         }
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtYear control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtYear_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtYear.Clear();
         }
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtTime control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtTime_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtTime.Clear();
         }
 
+        /// <summary>
+        /// Handles the GotKeyboardFocus event of the txtposYear control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyboardFocusChangedEventArgs"/> instance containing the event data.</param>
         private void txtposYear_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             txtposYear.Clear();
         }
-        
+
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtPos control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtPos_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if(txtPos.Text == "")
@@ -168,6 +219,11 @@ namespace Top2000App
             }
         }
 
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtTitel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtTitel_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if(txtTitel.Text == "")
@@ -176,6 +232,11 @@ namespace Top2000App
             }
         }
 
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtArtistN control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtArtistN_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (txtArtistN.Text == "") {
@@ -183,6 +244,11 @@ namespace Top2000App
             }
         }
 
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtYear control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtYear_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if(txtYear.Text == "")
@@ -191,6 +257,11 @@ namespace Top2000App
             }
         }
 
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtposYear control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtposYear_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if(txtposYear.Text == "")
@@ -199,6 +270,11 @@ namespace Top2000App
             }
         }
 
+        /// <summary>
+        /// Handles the IsKeyboardFocusedChanged event of the txtTime control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private void txtTime_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if(txtTime.Text == "")
